eee6f88361f0874a3da3d0ce7ef89878816702cf
Melina-vargas22 <mvargascobaxin@gmail.com>
2026-05-14 20:44:10 -0600

    Add files via upload

---
+++ #ordenar estudiantes por nombre.py
@@ -0,0 +1,29 @@
+#ordenar estudiantes por nombre 
+#Melina Vargas 
+
+class Estudiante:
+    def_int_(self, nombre, edad)
+       self.nombre = nombre
+       self.edad = edad
+       
+lista_Estudiante = []
+
+e1 = Estudiante("carlos" , 17)
+e2 = Estudiante("berenice" , 16)
+e3 = Estudiante("maria" , 17)
+e4 = Estudiante("marcos" , 16)
+e5 = Estudiante("jesus" , 16)
+
+lista_Estudiante.append(e1)
+lista_Estudiante.append(e2)
+lista_Estudiante.append(e3)
+lista_Estudiante.append(e4)
+lista_Estudiante.append(e5)
+
+def obtener_nombre(Estudiante):
+    return Estudiante.nombre
+
+ lista_estudiante.sort(Key=obtener.nombre)
+ 
+ for Estudiante in lista_Estudiantes:
+     print("nombre:" , estudiante.nombre, "edad:", estudiante.edad)
